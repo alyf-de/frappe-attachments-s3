085fc294d002f4642d0c3ea574caa06f4bb78e1c
Daniel Rose <26166128+dafrose@users.noreply.github.com>
2026-05-06 23:45:50 +0200

    fix(controller): add type hints to generate_file parameters

--- frappe_s3_attachment/controller.py
+++ frappe_s3_attachment/controller.py
@@ -227,7 +227,7 @@ def file_upload_to_s3(doc, method):
 
 
 @frappe.whitelist()
-def generate_file(key=None, file_name=None):
+def generate_file(key: str | None = None, file_name: str | None = None):
 	"""
 	Function to stream file from s3.
 	"""
